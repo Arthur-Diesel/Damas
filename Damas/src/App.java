207f7bf8486e2368cefb608d854fd42fd63efdd6
aluno <Aluno@DEVE009816.educacional.up>
2022-04-28 21:31:45 -0300

    New Pieces!

--- Damas/src/App.java
+++ Damas/src/App.java
@@ -8,6 +8,7 @@ public class App {
         ArrayList<Piece> Pieces = new ArrayList<>();
 
         // Colocar em um for \/
+        // ~ Brancas ~
         Pieces.add(new Pawn(0, 1, 'b'));
         Pieces.add(new Pawn(0, 3, 'b'));
         Pieces.add(new Pawn(0, 5, 'b'));
@@ -20,6 +21,21 @@ public class App {
         Pieces.add(new Dama(2, 3, 'B'));
         Pieces.add(new Dama(2, 5, 'B'));
         Pieces.add(new Dama(2, 7, 'B'));
+        
+        // ~ Pretas ~ 
+        Pieces.add(new Pawn(5, 0, 'p'));
+        Pieces.add(new Pawn(5, 2, 'p'));
+        Pieces.add(new Pawn(5, 4, 'p'));
+        Pieces.add(new Pawn(5, 6, 'p'));
+        Pieces.add(new Pawn(6, 1, 'p'));
+        Pieces.add(new Pawn(6, 3, 'p'));
+        Pieces.add(new Pawn(6, 5, 'p'));
+        Pieces.add(new Pawn(6, 7, 'p'));
+        Pieces.add(new Dama(7, 0, 'P'));
+        Pieces.add(new Dama(7, 2, 'P'));
+        Pieces.add(new Dama(7, 4, 'P'));
+        Pieces.add(new Dama(7, 6, 'P'));
+
 
         views.ShowBoard(Pieces);
     }
